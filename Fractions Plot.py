6733d7fe414f63aa54523df1826e91f972b244f5
hafzal99 <92127460+hafzal99@users.noreply.github.com>
2022-03-25 03:35:39 +0000

    Update Fractions Plot.py

--- Fractions Plot.py
+++ Fractions Plot.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 22 18:18:28 2022
-
-@author: hamza
-"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
